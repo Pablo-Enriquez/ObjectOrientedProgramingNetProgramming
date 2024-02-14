1834a0e4e92a9593b41c05cfe2bc2d56b9b51a0f
Pablo-Enriquez <36930307+Pablo-Enriquez@users.noreply.github.com>
2024-02-13 21:46:11 -0600

    Add files via upload

---
+++ CarClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace CarClass
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Welcome to the Car Inventory Application!");
+
+            bool continueLoop = true;
+            while (continueLoop)
+            {
+                Console.WriteLine("\nEnter the following car details:");
+
+                Console.Write("Model: ");
+                string model = Console.ReadLine();
+
+                Console.Write("Year: ");
+                int year = int.Parse(Console.ReadLine());
+
+                Console.Write("Price of the car: ");
+                double price = double.Parse(Console.ReadLine());
+
+                Car car = new Car(model, year, price);
+
+                Console.WriteLine("\nThe following are the car details:");
+                Console.WriteLine(car);
+
+                Console.WriteLine($"The estimated price after depreciation is: ${car.CalculateDepreciation():N2}");
+
+                Console.Write("\nWould you like to enter antoher car? (yes/no): ");
+                string response = Console.ReadLine().ToLower();
+                continueLoop = response == "yes";
+            }
+
+            Console.WriteLine("Thank you for using the Car Inventory Application! Come back soon!");
+    }
+}
---
+++ CarClass2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace EnriquezObjectOrientedProgramming
+{
+    //Gets the information for a car
+    public class Car
+    {
+        //These are the properties of a car
+        public string Model { get; set; }
+        public int Year { get; set; }
+        private double _price;
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value >= 0)
+                    _price = value;
+                else
+                    throw new ArgumentException("The price can't be negative");
+            }
+        }
+
+        //The following are the constructors
+        public Car()
+        {
+            //Default constructor
+        }
+
+        public Car(string model, int year, double price)
+        {
+            //These are the non default 
+            Model = model;
+            Year = year;
+            Price = price;
+        }
+
+        //These are the methods
+        public double CalculateDepreciation()
+        {
+            //This is with the assumption that the depreciation rate is 10% per year
+            int currentYear = DateTime.Now.Year;
+            int yearsOld = currentYear - Year;
+            double depreciation = Price * 0.1 * yearsOld;
+            return Price - depreciation;
+        }
+
+        public override string ToString()
+        {
+            return $"Model: {Model}, Year: {Year}, Price: ${Price:N2}";
+        }
+    }
+}
+
